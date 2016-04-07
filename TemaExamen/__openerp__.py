e5d583c0f22475953a96baef1c92b66be7d26d17
YPerezM <yago.p.m@hotmail.com>
2016-04-07 21:30:25 +0200

    Tema Examen Terminado

---
+++ TemaExamen/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Tema_Examen',
+    'description': 'Default website theme to showcase customization possibilities.',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'YPerezM',
+    'depends': ['website'],
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml', 
+        'views/assets.xml',
+        'views/snippets.xml'  
+    ],
+    'images': [
+        'static/description/cover.png',
+    ],
+    'application': False,
+}
